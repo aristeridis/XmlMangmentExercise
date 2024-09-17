fc4fcb6264ec9c93badcd704439e862250e1c67b
Alexandros aristeridis@hotmail.gr <aristeridis@hotmail.gr>
2024-09-17 23:21:20 +0300

    creation of a new xml from other xml with selected paragraphs and produces statistics

--- src/main/java/Service/Main.java
+++ src/main/java/Service/Main.java
@@ -1,13 +1,28 @@
 package Service;
 
 import Model.BookModel;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 		String txtFilePath = "txt/sample-lorem-ipsum-text-file.txt";
 		String xmlFilePath = "xml/book.xml";
+        String selectedXmlFile = "xml/paragraphs3To7.xml";
+		try {
+	List<String> paragraphs = SelectedParagraphsFromXml.SelectedParagraphsFromXml(xmlFilePath);
+        SelectedParagraphsFromXml.writeSelectedParagraphsToXml(selectedXmlFile, paragraphs);
+	  int numberOfParagraphs = SelectedParagraphsFromXml.countParagraphs(selectedXmlFile);
+        System.out.println("Number of paragraphs " + numberOfParagraphs);
 
+   
+		} catch (NullPointerException e) {
+			e.getMessage();
+		}
+		catch (Exception e) {
+			e.getMessage();
+		}
+       
 		try {
 			BookModel book = TxtToObject.TxtToObject(txtFilePath);
 
@@ -20,5 +35,7 @@ public class Main {
 		} catch (Exception e ) {
 			e.getMessage();
 		}
+		       // extractParagraphs("xml/book.xml", "xml/paragraphs3To7.xml");
+
 	}
 }
